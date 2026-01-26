1705a7c641df0da587f45c5dbdd7e442e3b3ae76
BohdanZubov <zubovbogdan41@gmail.com>
2026-01-26 15:16:31 +0200

    Initial commit

---
+++ scrypt.js
@@ -0,0 +1,27 @@
+// 1
+// const friends = ["Ivan", "Anton", "Oleksandr", "Tolik"];
+// for (let i = 0; i < friends; i++) {}
+// console.log(friends.join(" "));
+
+// const cards = [
+//   "Карточка 1",
+//   "Карточка 2",
+//   "Карточка 3",
+//   "Карточка 4",
+//   "Карточка 5",
+// ];
+
+// const cardToRemove = "Карточка 3";
+// let index = cards.indexOf(cardToRemove);
+// cards.splice(index, 1);
+
+// const cardToAdd = "Карточка 6";
+// cards.splice(cards.length, 0, cardToAdd);
+
+// const cardToUpdate = "Карточка 4"
+// let indexToUpdate = cards.indexOf(cardToUpdate)
+
+// if( cardToUpdate !== -1){
+//   cards.splice(indexToUpdate, 1, "Нова карточка 4")
+// }
+// console.log(cards);
